7316834b715a8bddd4de9bef1a7fbc0ab87e271a
Mert Paker <mertpaker@gmail.com>
2019-07-11 21:13:50 +0100

    HTTP Load Balancer

---
+++ example-lb-http/variables.tf
@@ -0,0 +1,19 @@
+variable "group1_size" {
+  default = "2"
+}
+
+variable "group2_size" {
+  default = "2"
+}
+
+variable "group1_region" {
+  default = "us-west1"
+}
+
+variable "group2_region" {
+  default = "us-east1"
+}
+
+variable "network_name" {
+  default = "tf-lb-http-basic"
+}
